6d33fb75764158b4dfeb6774b2426118266c61a7
Ahmed Abdalla <ahmad19526@gmail.com>
2016-06-20 12:56:39 -0400

    adds script to start crossbar

--- start_crossbar.py
+++ start_crossbar.py
@@ -1,4 +1,11 @@
+import multiprocessing
+
+
 from crossbar.controller.cli import run
 
 if __name__ == '__main__':
-    run(args=["start"])
+    p = multiprocessing.Process(
+        target=lambda: run(args=["start"])
+    )
+    p.start()
+    p.join()
