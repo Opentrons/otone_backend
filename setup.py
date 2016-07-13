86feb054d9255d7709af770cae90cbc93dcc5f64
Ahmed Abdalla <ahmad19526@gmail.com>
2016-07-12 23:09:53 -0400

    forces __init__.py creation for zope.interface package

--- setup.py
+++ setup.py
@@ -4,21 +4,31 @@ import glob
 import os
 import sys
 
-import py2exe
+try:
+    import py2exe
+except:
+    pass
 
-sys.path.append(
-	(os.path.dirname(__file__) or os.getcwd()) + "\\backend"
-)
 
-print(sys.path)
-includes = [
-	'head',
-]
+if sys.argv[1] == "py2exe":
+    # Add the backend dir to the sys path so that py2exe can package it
+    sys.path.append(
+        (os.path.dirname(__file__) or os.getcwd()) + "\\backend"
+    )
+
+    # Create __init__.py file in the top level zope dir so py2exe can import it
+    # http://stackoverflow.com/questions/7816799
+    import zope
+    open(
+        os.path.join(zope.__path__[0], '__init__.py'), 'a'
+    ).close()
+
 
 packages = [
 	'twisted',
 	'zope.interface',
 ]
+
 setup(
 	console=["backend/otone_client.py"],
 	data_files=[
@@ -27,7 +37,7 @@ setup(
 	options={
 		'py2exe': {
 			'packages': packages,
-			'includes': includes,
+			'includes': [],
 			'excludes': ["six.moves.urllib.parse"]
 		}
 	}
