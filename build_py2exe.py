3c6d5b9fe99a01aeda6d7e9fc606fe9867cfaf3a
Ahmed Abdalla <ahmad19526@gmail.com>
2016-07-13 16:32:04 -0400

    attempts to setup python based build script

---
+++ build_py2exe.py
@@ -0,0 +1,52 @@
+# setup.py
+from distutils.core import setup
+import glob
+import os
+import sys
+
+try:
+    import py2exe
+except:
+    pass
+
+
+script_tag = '[otone_backend build] '
+
+
+# Add the backend dir to the sys path so that py2exe can package it
+sys.path.append(
+    (os.path.dirname(__file__) or os.getcwd()) + "\\backend"
+)
+
+
+def get_py2exe_packages():
+    return ['twisted','zope.interface']
+
+def build_exe(args):
+    setup(
+	console=["backend/otone_client.py"],
+	data_files=[
+	    ('data', glob.glob('backend\\data\\*')),
+	    ("Microsoft.VC90.CRT", glob.glob("msvcm90\*.*")),
+	    ("", ["C:\\Windows\\System32\\msvcr100.dll"])
+	],
+	options={
+	    'py2exe': {
+		'packages': get_py2exe_packages(),
+		'includes': [],
+		'excludes': ["six.moves.urllib.parse"]
+	    }
+	},
+        script_args=args
+    )
+
+def main():
+    print(script_tag + "Building otone_backend using py2exe.")
+    if len(sys.argv) <= 1:
+        build_exe(["py2exe"])
+    else:
+        build_exe()
+
+
+if __name__ == '__main__':
+    main()
